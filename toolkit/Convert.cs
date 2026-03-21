43bc3f02dc8eeddfc08fe9c7c0d505767b7a41c3
boxrocket <boxrocket6803@gmail.com>
2026-03-21 16:12:17 -0500

    generic converter

--- toolkit/Convert.cs
+++ toolkit/Convert.cs
@@ -2,7 +2,25 @@
 
 public class Convert {
 	public static void Main(string[] args) {
-		
-		Console.WriteLine("test");
+		List<string> argacc = [.. args];
+		Console.WriteLine("-- boxrend generalized format converter utility --");
+		if (argacc.Count == 0) {
+			Console.WriteLine("cmd args");
+			Console.WriteLine(" <input file>");
+		}
+		while (!File.Exists(argacc.ElementAtOrDefault(0))) {
+			if (argacc.Count > 0) {
+				Console.WriteLine($"{argacc[0]} is not a valid file");
+				argacc.RemoveAt(0);
+			}
+			Console.Write("input file: ");
+			argacc.Insert(0, Console.ReadLine().Trim());
+		}
+		if (bmdl.Convert.Supports(argacc[0]))
+			bmdl.Convert.Main([.. argacc]);
+		else if (bseq.Convert.Supports(argacc[0]))
+			bseq.Convert.Main([.. argacc]);
+		else if (btex.Convert.Supports(argacc[0]))
+			btex.Convert.Main([.. argacc]);
 	}
 }
